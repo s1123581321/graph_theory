3b885bd4204b6f8c6bb1501ad24b09a560afc296
Stan <stan.evans@email.com>
2023-03-19 21:10:09 +0000

    General graph/path method class

--- py/script.py
+++ py/script.py
@@ -1,58 +1,85 @@
 from example_graphs import *
 
-def graph_traversal(graph, start_node, mode='breadth'):
-    '''
-    Returns the traversal of the input graph
-    graph     : Input graph in adjacency list representation
-    start_node: Start node/vertex for traversal
-    mode      : Traversal mode 'depth' or 'breadth', defaults to 'depth'
-    '''
-    stack = [start_node]
-    mode_idx = {'depth': -1, 'breadth': 0}[mode]
-    r = []
-    while(len(stack)>0):
-        node = stack.pop(mode_idx)
-        r.append(node)
-        for neighbour in graph[node]:
-            if (not neighbour in r) and (not neighbour in stack):
-                stack.append(neighbour)
-    return r
+class graph_adj:
+	'''
+	Adjacency list directed/undirected graph representation class with path methods
+	'''
+	def __init__(self, graph, graph_rep='adj'):
+		self.adj = graph
+	
+	def graph_traversal(self, start_node, mode='breadth'):
+		'''
+		Returns the traversal of the graph
+		start_node: Start node/vertex for traversal
+		mode      : Traversal mode 'depth' or 'breadth', defaults to 'depth'
+		'''
+		stack = [start_node]
+		mode_idx = {'depth': -1, 'breadth': 0}[mode]
+		traversal = []
+		while(len(stack)>0):
+			node = stack.pop(mode_idx)
+			traversal.append(node)
+			for neighbour in self.adj[node]:
+				if (not neighbour in traversal) and (not neighbour in stack):
+					stack.append(neighbour)
+		return traversal
+
+	def find_path_s(self, start_node, end_node, return_type='all'):
+		'''
+		Returns the path(s) from the start node to the end node
+		start_node : Start node of path
+		end_node   : End node of path
+		return_type: Defaults to 'all' to return all paths, 'shortest' returns only the shortest path
+		'''
+		if start_node == end_node:
+			return [[start_node]]
+		return_shortest = {'shortest': True, 'all': False}[return_type]
+		path_found = False
+		no_path = False
+		path_list = [[start_node]]
+		temp_path_list = []
+		good_paths = []
+		while (not (path_found and return_shortest)) and (not no_path):
+			for path in path_list:
+				paths = []
+				for neighbour in self.adj[path[-1]]:
+					if not neighbour in path:
+						paths.append(path + [neighbour])
+						if neighbour == end_node:
+							path_found = True
+							good_paths.append(path + [neighbour])
+				temp_path_list += paths
+			if temp_path_list == []:
+				no_path = True
+			else:
+				path_list = temp_path_list
+				temp_path_list = []
+		return good_paths
+
+	def pathExists(self, start_node, end_node, mode='breadth'):
+		'''
+		Returns True/False for if start_node to end_node path exists in the intput graph
+		start_node: Start node/vertex
+		end_node  : End node/vertex
+		mode      : Traversal mode 'depth' or 'breadth', defaults to 'depth'
+		'''
+		if start_node == end_node:
+			return True
+		stack = [start_node]
+		mode_idx = {'depth': -1, 'breadth': 0}[mode]
+		r = []
+		while(len(stack)>0):
+			node = stack.pop(mode_idx)
+			r.append(node)
+			if end_node in self.adj[node]:
+				return True
+			for neighbour in self.adj[node]:
+				if (not neighbour in r) and (not neighbour in stack):
+					stack.append(neighbour)
+		return False
 
 
-def find_path_s(graph, start_node, end_node, return_type='all'):
-    '''
-    Returns the path(s) from the start node to the end node
-    graph      : Input graph in adjacency list representation
-    start_node : Start node of path
-	end_node   : End node of path
-	return_type: Defaults to 'all' to return all paths, 'shortest' returns only the shortest path
-    '''
-    if start_node == end_node:
-        return [[start_node]]
-    return_shortest = {'shortest': True, 'all': False}[return_type]
-    path_found = False
-    no_path = False
-    path_list = [[start_node]]
-    temp_path_list = []
-    good_paths = []
-    while (not (path_found and return_shortest)) and (not no_path):
-        for path in path_list:
-            paths = []
-            for neighbour in graph[path[-1]]:
-                if not neighbour in path:
-                    paths.append(path + [neighbour])
-                    if neighbour == end_node:
-                        path_found = True
-                        good_paths.append(path + [neighbour])
-            temp_path_list += paths
-        if temp_path_list == []:
-            no_path = True
-        else:
-            path_list = temp_path_list
-            temp_path_list = []
-    return good_paths
-
-
+#Undirected graph specific functions
 def connected_components(graph, mode='breadth'):
     '''
     Returns all traversals of components of an undirected graph
@@ -83,30 +110,7 @@ def largest_connected_component_s(graph, mode='breadth'):
     return largest_component_s
 
 
-def pathExists(graph, start_node, end_node, mode='breadth'):
-    '''
-    Returns True/False for if start_node to end_node path exists in the intput graph
-    graph     : Input graph in adjacency list representation
-    start_node: Start node/vertex
-    end_node  : End node/vertex
-    mode      : Traversal mode 'depth' or 'breadth', defaults to 'depth'
-    '''
-    if start_node == end_node:
-        return True
-    stack = [start_node]
-    mode_idx = {'depth': -1, 'breadth': 0}[mode]
-    r = []
-    while(len(stack)>0):
-        node = stack.pop(mode_idx)
-        r.append(node)
-        if end_node in graph[node]:
-            return True
-        for neighbour in graph[node]:
-            if (not neighbour in r) and (not neighbour in stack):
-                stack.append(neighbour)
-    return False
-
-
+#Graph representation functions
 def edges_to_adj_list(edge_list):
     '''
     Returns the adjacency representation of an undirected graph from an input of edge list
@@ -122,11 +126,6 @@ def edges_to_adj_list(edge_list):
         adj_list[edge[1]].append(edge[0])
     return adj_list
 
-
-
-
-
-
 def adj_to_mat_rep(adj_rep):
     '''
     Returns the matrix representation of an adjacency list representation
@@ -138,7 +137,6 @@ def adj_to_mat_rep(adj_rep):
         for vertex in adj_rep[node]:
             mat_rep[node][vertex] = 1
     return mat_rep
-
 
 def print_mat_rep(mat_rep):
     '''
